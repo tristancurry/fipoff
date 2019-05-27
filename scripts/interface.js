aa22b5be9ed6d474f848323355218a99f50579db
Tristan Miller <tristan.j.miller@gmail.com>
2019-05-27 16:17:12 +0930

    Initial commit - boilerplatey goodness

---
+++ scripts/interface.js
@@ -0,0 +1,13 @@
+//Interface.js
+
+//TODO:initialise button states
+
+//Add event listeners
+
+
+
+
+
+function toggleDisplay(elm){
+	elm.classList.toggle('show');
+}
---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//main.js - main program code
+
+
+//modify a value if it exceeds limits
+function constrain(n, min, max){
+	if(n > max){n = max;} else if(n < min){n = min;}
+	return n;	
+}

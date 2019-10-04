693c3fbe8e9cff98df113131942eed9d3a090496
Tristan Miller <tristan.j.miller@gmail.com>
2019-10-04 16:00:55 +0930

    Update system00.js
    All devices are now added for this system!

--- systems/system00/system00.js
+++ systems/system00/system00.js
@@ -11,6 +11,142 @@ system = {
 			//blockplan - detector dimensions
 			children: [
 				{
+					name: 'Loop 1',
+					shname: 'l1',
+					category: 'circuit',
+					colour: 'magenta',
+					loop: 1,
+					zone: 1,
+					page_number: 1,
+					children:[
+						{
+							name: 'Front door',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '309px', y:'610px'}
+						},
+						{
+							name: 'Station office',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '230px', y:'699px'}
+						},
+						{
+							name: 'Station office (ceiling)',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '199px', y:'699px'},
+							concealed: true
+						},
+						{
+							name: "Officers' quarters, hallway",
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '139px', y:'658px'}
+						},
+						{
+							name: "Officers' quarters, bedroom 1",
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '102px', y:'697px'}
+						},
+						{
+							name: "Officers' quarters, bedroom 2",
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '99px', y:'639px'}
+						},
+						{
+							name: 'Mess, kitchen',
+							category: 'det',
+							type: 'thermal',
+							subtype: 'th',
+							addressable: true,
+							pos: {x: '49px', y:'538px'}
+						},
+						{
+							name: 'Mess, dining',
+							category: 'det',
+							type: 'thermal',
+							subtype: 'th',
+							addressable: true,
+							pos: {x: '122px', y:'534px'}
+						},
+						{
+							name: 'Mess, dining (ceiling)',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '120px', y:'558px'},
+							concealed: true
+						},
+						{
+							name: 'Hallway, station entrance',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '188px', y:'609px'},
+						},
+						{
+							name: "Officers' bathroom",
+							category: 'det',
+							type: 'thermal',
+							subtype: 'th',
+							addressable: true,
+							pos: {x: '228px', y:'559px'}
+						},
+						{
+							name: 'Radio equipment room',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '263px', y:'560px'},
+						},
+						{
+							name: 'Control room',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '320px', y:'558px'},
+						},
+						{
+							name: 'Lounge room (ceiling)',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '257px', y:'492px'},
+							concealed: true
+						},
+						{
+							name: 'Lounge room',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '259px', y:'464px'},
+						},
+					]
+					
+				},
+				
+				{
 					name: 'Loop 2',
 					shname: 'l2',
 					category: 'circuit',
@@ -45,7 +181,7 @@ system = {
 							pos: {x: '243px', y:'309px'}
 						},
 						{
-							name: 'Crew quarters south 1',
+							name: 'Crew quarters south, bedroom 1',
 							category: 'det',
 							type: 'smoke',
 							subtype: 'pe',
@@ -53,7 +189,7 @@ system = {
 							pos: {x: '153px', y:'300px'}
 						},
 						{
-							name: 'Crew quarters south 2',
+							name: 'Crew quarters south, bedroom 2',
 							category: 'det',
 							type: 'smoke',
 							subtype: 'pe',
@@ -61,7 +197,7 @@ system = {
 							pos: {x: '93px', y:'298px'}
 						},
 						{
-							name: 'Crew quarters south 3',
+							name: 'Crew quarters south, bedroom 3',
 							category: 'det',
 							type: 'smoke',
 							subtype: 'pe',
@@ -69,7 +205,7 @@ system = {
 							pos: {x: '35px', y:'300px'}
 						},
 						{
-							name: 'Crew quarters south hallway',
+							name: 'Crew quarters south, hallway',
 							category: 'det',
 							type: 'smoke',
 							subtype: 'pe',
@@ -77,7 +213,7 @@ system = {
 							pos: {x: '88px', y:'250px'}
 						},
 						{
-							name: 'Crew quarters south hallway (ceiling)',
+							name: 'Crew quarters south, hallway (ceiling)',
 							category: 'det',
 							type: 'smoke',
 							subtype: 'pe',
@@ -93,7 +229,7 @@ system = {
 							addressable: true,
 							pos: {x: '65px', y:'189px'},
 						},
-{
+						{
 							name: 'Crew quarters bathroom east',
 							category: 'det',
 							type: 'thermal',
@@ -101,6 +237,80 @@ system = {
 							addressable: true,
 							pos: {x: '135px', y:'189px'},
 						},						
+						{
+							name: 'Crew quarters north, bedroom 3',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '37px', y:'77px'},
+						},
+						{
+							name: 'Crew quarters north, bedroom 2',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '94px', y:'78px'},
+						},
+						{
+							name: 'Crew quarters north, hallway',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '88px', y:'127px'},
+						},
+						{
+							name: 'Crew quarters north, hallway (ceiling)',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '152px', y:'126px'},
+							concealed: true
+						},
+						{
+							name: 'Crew quarters north, bedroom 1',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '154px', y:'76px'},
+						},
+						{
+							name: 'Northern entryway',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '202px', y:'137px'},
+						},
+						{
+							name: 'Store room',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '288px', y:'123px'},
+						},
+						{
+							name: 'Store room (ceiling)',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '290px', y:'157px'},
+							concealed: true,
+						},
+						{
+							name: 'Laundry/BA Area',
+							category: 'det',
+							type: 'smoke',
+							subtype: 'pe',
+							addressable: true,
+							pos: {x: '347px', y:'159px'},
+						},
 					]
 					
 				},

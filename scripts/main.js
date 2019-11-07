d6076bfbe390eb791d87bfaba8384a859e593fe0
Tristan Miller <tristan.j.miller@gmail.com>
2019-11-07 20:25:50 +1030

    Building menu system
    menu system for initialising an alarm scenario

--- scripts/main.js
+++ scripts/main.js
@@ -127,30 +127,30 @@ function addLeadingZero(str){
 
 
 
-buildSystem(system);
-buildZoneLists();
-console.log(sysObjects);
-
-console.log(zones);
-buildFips();
-console.log(sysObjectsByCategory);
-
-let fipList = sysObjectsByCategory['fip'];
-// needed to loop backwards so that the alarm counts were correct for FIPs closer to the main FirePanel
-// TODO: make the title of the blockplan depend on a variable stored with the FIP, not the entire system
-for (let l = fipList.length, i = l - 1; i >= 0; i--) {
-	let thisFip = fipList[i];
-		thisFip.triggerRandomAlarms(3);
-	 thisFip.assignStatusIds();
-	 thisFip.displayStatus();
-
-	for (let i = 0, l = thisFip.deviceList.length; i < l; i++){
-		thisFip.updateDeviceImagePath(thisFip.deviceList[i]);
-	}
-	//keep the clock running. When we have multiple FIPs, do them all at once
-	//TODO: make it so that there is only one setInterval, with one function that updates all FIP displays.
-	window.setInterval(function(){thisFip.displayStatus();}, 500);
-}
+// buildSystem(system);
+// buildZoneLists();
+// console.log(sysObjects);
+//
+// console.log(zones);
+// buildFips();
+// console.log(sysObjectsByCategory);
+//
+// let fipList = sysObjectsByCategory['fip'];
+// // needed to loop backwards so that the alarm counts were correct for FIPs closer to the main FirePanel
+// // TODO: make the title of the blockplan depend on a variable stored with the FIP, not the entire system
+// for (let l = fipList.length, i = l - 1; i >= 0; i--) {
+// 	let thisFip = fipList[i];
+// 		thisFip.triggerRandomAlarms(3);
+// 	 thisFip.assignStatusIds();
+// 	 thisFip.displayStatus();
+//
+// 	for (let i = 0, l = thisFip.deviceList.length; i < l; i++){
+// 		thisFip.updateDeviceImagePath(thisFip.deviceList[i]);
+// 	}
+// 	//keep the clock running. When we have multiple FIPs, do them all at once
+// 	//TODO: make it so that there is only one setInterval, with one function that updates all FIP displays.
+// 	window.setInterval(function(){thisFip.displayStatus();}, 500);
+// }
 
 
 function buildSystem (sys) {
